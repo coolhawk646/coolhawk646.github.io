5608c23678b0795681b6cca3a3b23a4356001098
coolhawk646 <coolhawk646@gmail.com>
2017-01-29 10:57:59 -0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-9149302403736546",
+    enable_page_level_ads: true
+  });
+</script>
+
 console.log('This would be the main JS file.');
